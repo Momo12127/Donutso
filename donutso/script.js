ecec6e3a3a943548035c60baedd564081d915515
Momo12127 <mohamedmostafa12127@gmail.com>
2025-08-22 22:09:44 +0300

    Update script.js

--- donutso/script.js
+++ donutso/script.js
@@ -95,8 +95,7 @@ function showToast(message = "✔️ Added to cart!") {
   }, 2000);
 }
 
-
-  const menuToggle = document.querySelector('.menu-toggle');
+ const menuToggle = document.querySelector('.menu-toggle');
   const navLinks = document.querySelector('.nav-links');
 
   menuToggle.addEventListener('click', () => {
